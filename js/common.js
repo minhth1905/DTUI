bbffbd84aad4f6644467ed14a6060fdebb22d8f9
nvn01234 <nguyenvannhat152@gmail.com>
2016-10-12 00:15:06 +0700

    massive change

--- js/common.js
+++ js/common.js
@@ -25,6 +25,35 @@ $.getJSON("json/mockups.json", function (mockups) {
     onMockupLoaded(mockups);
 });
 
+function randomMockup(mockups, size) {
+    var ids = [];
+    for (var box = 0; box < size; box++) {
+        var rand_id = randomMockupId(mockups, ids);
+        ids.push(rand_id);
+        var mockup = findMockup(mockups, rand_id);
+        $("#template").append($("<div>").addClass("shakira-box").attr("mockup-id", mockup.id)
+            .append($("<img>").addClass("preview").attr("src", getPreviewImage(mockup)))
+            .append($("<div>").addClass("icon_0")
+                .append($("<img>").attr("src", getCategoryIcon24x24(mockup))))
+            .append($("<div>").addClass("shakira-content")
+                .append($("<h2>").addClass("shakira-header").text(mockup.short_name))
+                .append($("<p>").addClass("shakira-body")
+                    .append($("<i>").addClass("fa fa-thumbs-o-up"))
+                    .append("13,86")
+                    .append($("<i>").addClass("fa fa-eye"))
+                    .append("12,56")
+                    .append($("<i>").addClass("fa fa-download"))
+                    .append("11,54")
+                )
+            )
+        )
+    }
+    $(".shakira-box").click(function () {
+        var id = $(this).attr("mockup-id");
+        window.location.href = "details.html?id=" + id;
+    });
+}
+
 function randomMockupId(mockups, excludes) {
     var rand_id;
     do {
--- js/search.js
+++ js/search.js
@@ -1,61 +1,101 @@
-/**
- * Created by nguye on 10/10/2016.
- */
 $('.show-menu').click(function () {
     $('#menu-phone').slideToggle('slow');
 });
 
-function onMockupLoaded(mockups) {
-    var key_param = GetURLParameter("key");
-    if (!!key_param) {
-        $(".value-search").val(decodeURI(key_param.trim()));
-    }
+var _mockups;
 
-    var category_param = GetURLParameter("category");
-    $("#menu").find("li").each(function () {
-        var category = $(this).attr("category");
-        if (category == category_param) {
+function onMockupLoaded(mockups) {
+    _mockups = mockups;
+    var length = mockups.length;
+    $(".number-template").text(length + " ");
+    randomMockup(mockups, 20);
+}
+
+function reload() {
+    $("#template").empty();
+    randomMockup(_mockups, 20);
+}
+
+var menu = $("#menu");
+function selectCategory(category) {
+    menu.find("li[category=\"" + category + "\"]").each(function () {
+        $(this).addClass("selected");
+    });
+    menu.find("li").each(function () {
+        var _category = $(this).attr("category");
+        if (category == _category) {
             $(this).addClass("selected");
         } else {
             $(this).removeClass("selected");
         }
-        var key = $(".value-search").val();
-        $(this).find("a").attr("href", "Search.html?category=" + category + "&key=" + encodeURI(key));
-    });
-
-    $("#tag-list").find("a").each(function () {
-        var key = $(this).text();
-        $(this).attr("href", "Search.html?category=" + category_param + "&key=" + encodeURI(key));
-    });
-
-    var p_param = GetURLParameter("p");
-    if (!!p_param) {
-        p_param = 1;
-    }
-    if (p_param == 1) {
-        $("#first-page").addClass("disabled");
-        $("#previous-page").addClass("disabled");
-        $("#next-page").removeClass("disabled");
-        $("#last-page").removeClass("disabled");
-    } else if (p_param > 1 && p_param < 10) {
-        $("#first-page").removeClass("disabled");
-        $("#previous-page").removeClass("disabled");
-        $("#next-page").removeClass("disabled");
-        $("#last-page").removeClass("disabled");
-    } else if (p_param == 10) {
-        $("#first-page").removeClass("disabled");
-        $("#previous-page").removeClass("disabled");
-        $("#next-page").addClass("disabled");
-        $("#last-page").addClass("disabled");
-    }
-    $(".page-number").each(function () {
-        var p = $(this).find("a").text();
-        if (p == p_param) {
-            $(this).addClass("current-page");
-            $(this).find("a").attr("href", null);
-        } else {
-            $(this).removeClass("current-page");
-            $(this).find("a").attr("href", "Search.html?p=" + p + "&category=" + category_param + "&key=" + key_param);
-        }
     });
 }
+
+var key = GetURLParameter("key");
+if (!!key) {
+    $(".value-search").val(decodeURI(key));
+}
+
+var category = GetURLParameter("category");
+if (!!category) {
+    selectCategory(category);
+}
+
+menu.find("li").click(function () {
+    var _category = $(this).attr("category");
+    if (!!_category) {
+        selectCategory(_category);
+        reload();
+        history.pushState({}, null, "Search.html");
+    }
+});
+
+var paging_holder = $("#paging-holder");
+function selectPage(page) {
+    paging_holder.find(".current").removeClass("current");
+    $(page).addClass("current");
+    var p = $(page).find("a").text();
+    if (p == 1) {
+        paging_holder.find(".page-jump.back").addClass("disabled");
+        paging_holder.find(".page-jump.next").removeClass("disabled");
+    } else if (p == 10) {
+        paging_holder.find(".page-jump.back").removeClass("disabled");
+        paging_holder.find(".page-jump.next").addClass("disabled");
+    } else {
+        paging_holder.find(".page-jump.back").removeClass("disabled");
+        paging_holder.find(".page-jump.next").removeClass("disabled");
+    }
+    reload();
+    $("html, body").animate({scrollTop: 0}, "slow");
+}
+
+paging_holder.find("li").click(function () {
+    if ($(this).hasClass("page-number") && !$(this).hasClass("current")) {
+        selectPage(this);
+    } else if ($(this).hasClass("page-jump") && !$(this).hasClass("disabled")) {
+        var id = $(this).attr("id");
+        if (id == "first-page") {
+            selectPage(paging_holder.find(".page-number").first());
+        } else if (id == "previous-page") {
+            selectPage(paging_holder.find(".current").prev());
+        } else if (id == "next-page") {
+            selectPage(paging_holder.find(".current").next());
+        } else if (id == "last-page") {
+            selectPage(paging_holder.find(".page-number").last());
+        }
+    }
+});
+
+$(".tag-list").find("a").click(function () {
+    var key = $(this).text();
+    $(".value-search").val(key);
+    reload();
+});
+
+$("#sortby").find("a").click(function () {
+    reload();
+});
+
+$("#search").find("a").click(function () {
+    reload();
+});
